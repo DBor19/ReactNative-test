06b5751587ac44b4a0ea33a93da55ff571fef8e2
Daniel Nogueira <daniel.nogueira.filho@gmail.com>
2024-06-23 18:49:05 -0300

    teste aula funcionando

---
+++ banco/Create.js
@@ -0,0 +1,17 @@
+import * as SQLite from 'expo-sqlite';
+
+export const create = async () => {
+    try {
+        db = await SQLite.openDatabaseAsync('databaseName');
+        result = await db.runAsync(`
+                            CREATE TABLE IF NOT EXISTS contacts (id INTEGER PRIMARY KEY NOT NULL, name TEXT NOT NULL, phone TEXT);
+        `);
+        if (result.changes > 0)
+            console.log("[LOG] Query executed");
+        else
+            console.log("[LOG] Empty result retrieved from query");
+    } catch (error) {
+        console.log(error);
+    }
+    return db;
+}
